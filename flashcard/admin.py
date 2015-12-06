4e9c3ebdafd93438ea99ac8be95028aa4273a963
carleton <carleton@new-host.home>
2015-12-06 12:42:10 -0500

    add import/export integration with flashcard models

--- flashcard/admin.py
+++ flashcard/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from .models import CardGenerator
 
-admin.site.register(CardGenerator)
+class CGResource(resources.ModelResource):
+    class Meta:
+        model = CardGenerator
+
+class CGAdmin(ImportExportModelAdmin):
+    resource_class = CGResource
+    pass
+
+
+admin.site.register(CardGenerator,CGAdmin)
